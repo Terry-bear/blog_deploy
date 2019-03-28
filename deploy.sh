97c88ba151ac56dbdf5fb393c665f49343c73266
Terry-zh <496971418@qq.com>
2019-03-28 17:10:00 +0800

    [add] deploy

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# deploy
+# openssl aes-256-cbc -K $encrypted_1ddk1199d526_key -iv $encrypted_1ddk1199d526_iv -in CI/id_rsa.enc -out ~/.ssh/id_rsa -d
+# chmod 600 ~/.ssh/id_rsa
+# eval $(ssh-agent)
+# ssh-add ~/.ssh/id_rsa
+# git config --global user.name "your name"
+# git config --global user.email "your email"
+# git clone <repository>
+# cd <repository directory>
+# cp -R ../public/* .
+# git add .
+# git commit -m "built blog"
+# git push origin master
+echo "haha"
+
+exit 0
