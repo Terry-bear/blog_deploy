e2e6fcba12bfb11bbcfc24e326bb6ec0cc54b4a1
Terryzh <496971418@qq.com>
2019-04-01 20:11:11 +0800

    [server]

--- CI/deploy.sh
+++ CI/deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # deploy
-openssl aes-256-cbc -K $encrypted_e206ebe4192c_key -iv $encrypted_e206ebe4192c_iv -in CI/deploy_rsa.enc -out ~/.ssh/deploy_rsa -d
-chmod 600 ~/.ssh/deploy_rsa
+openssl aes-256-cbc -K $encrypted_e206ebe4192c_key -iv $encrypted_e206ebe4192c_iv -in CI/id_rsa -out ~/.ssh/id_rsa -d
+chmod 600 ~/.ssh/id_rsa
 eval $(ssh-agent)
-ssh-add ~/.ssh/deploy_rsa
+ssh-add ~/.ssh/id_rsa
 git config --global user.name "terryzh"
 git config --global user.email "496971418@qq.com"
 git clone git@github.com:t496971418/my_blog.git
