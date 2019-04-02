d27cf9c9e0848f15d636248ba50164e0695b8ac1
Terryzh <496971418@qq.com>
2019-04-02 17:37:00 +0800

    [server]

--- CI/deploy.sh
+++ CI/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # deploy
 openssl aes-256-cbc -K $encrypted_e206ebe4192c_key -iv $encrypted_e206ebe4192c_iv -in CI/secrets.tar.enc -out ~/.ssh/secrets.tar -d
-tar xvf ~/.ssh/secrets.tar
+tar xvf ~/.ssh/secrets.tar -C ~/.ssh
 chmod 600 ~/.ssh/server
 chmod 600 ~/.ssh/id_rsa
 eval $(ssh-agent)
