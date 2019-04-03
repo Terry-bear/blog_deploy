8143d2c0a4f28bb6952bcf78df27ca9caf0c20c2
Terryzh <496971418@qq.com>
2019-04-03 14:24:43 +0800

    [server]

--- CI/deploy.sh
+++ CI/deploy.sh
@@ -2,20 +2,9 @@
 # deploy
 openssl aes-256-cbc -K $encrypted_e206ebe4192c_key -iv $encrypted_e206ebe4192c_iv -in CI/secrets.tar.enc -out ~/.ssh/secrets.tar -d
 tar xvf ~/.ssh/secrets.tar -C ~/.ssh
-chmod 600 ~/.ssh/server
+chmod 600 ~/.ssh/server.pub
 chmod 600 ~/.ssh/id_rsa
 pwd
-cat <<EOF > /home/travis/.ssh/config  
-Host github.com
-User 496971418@qq.com
-PreferredAuthentications publickey
-IdentityFile ~/.ssh/id_rsa
-
-Host $IP
-User terryzh
-PreferredAuthentications publickey
-IdentityFile ~/.ssh/server
-EOF
 eval $(ssh-agent)
 ssh-add ~/.ssh/id_rsa
 git config --global user.name "terryzh"
@@ -28,10 +17,10 @@ git commit -m "[Ops]Travis built blog"
 git push origin master
 
 # upload master server
-cat ~/.ssh/config
-ssh-add ~/.ssh/server
+cat ~/.ssh/server.pub > ~/.ssh/authorized_keys
 cd ..
 ls -lrt
 chmod 777 my_blog
-# scp -o StrictHostKeyChecking=no -r my_blog/*  root@$IP:/var/www/html
+ssh root@39.104.123.222
+ls
 exit 0
